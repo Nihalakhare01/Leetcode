76acd58f990d88517df31843fc4ae03b34174970
Nihal Akhare <116178876+Nihalakhare01@users.noreply.github.com>
2024-08-13 21:14:49 +0530

    Time: 1 ms (99.98%), Space: 45.3 MB (90.84%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-       int fp = n-1;
+        int fp = n-1;
 
-       for(int i = n-2; i>=0; i--){
+        for(int i = n-2; i>=0; i--){
             if(i + nums[i] >= fp){
                 fp = i;
             }
         }
 
-        return fp == 0;
+        // if(fp == 0)
+        //     return true;
+        // else 
+        //     return false;
 
+        return fp == 0;
     }
 }
