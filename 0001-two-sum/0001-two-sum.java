c6eab3f79028168f7545220ef7c2e1fa8bf6a754
Nihal Akhare <116178876+Nihalakhare01@users.noreply.github.com>
2024-08-25 14:43:21 +0530

    Time: 2 ms (98.19%), Space: 45 MB (18.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,43 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int i=0,j=0;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-          for( i=0; i<nums.length-1; i++){
-            for( j=i+1; j<nums.length;j++ ){
+        int diff = 0;
 
-                if(nums[i] + nums[j] == target)
-                    return new int[]{i,j};
-                }
-           }
-         return new int[]{};
+        for(int i=0; i<nums.length; i++){
+            diff = target - nums[i];
+            if(map.containsKey(diff))
+                return new int[]{map.get(diff), i};
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+        
+        
+        // int i=0,j=0;
+
+        //   for( i=0; i<nums.length-1; i++){
+        //     for( j=i+1; j<nums.length;j++ ){
+
+        //         if(nums[i] + nums[j] == target)
+        //             return new int[]{i,j};
+        //         }
+        //    }
+        //  return new int[]{};
+
+        //  int lp =0, rp = nums.length-1;
+
+        //  while(lp < rp){
+
+        //     if(nums[lp] + nums[rp] == target){
+        //          return new int[]{lp, rp};
+        //     }else if(nums[lp] + nums[rp] < target){ 
+        //         rp--;
+        //     }else{ 
+        //         lp++;
+        //     }    
+        //  }
+        //  return new int[]{};
+
+        
     }
 }
